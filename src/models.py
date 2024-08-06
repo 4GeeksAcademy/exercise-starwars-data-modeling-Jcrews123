a134f2aba9ef9ef26617df7889f03242ac9c644d
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-08-06 00:12:02 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -7,12 +7,26 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    user_uid = Column(Integer, ForeignKey("user.uid"), primary_key=True, )
+    people_uid = Column(Integer, ForeignKey('people.uid'))
+    planets_uid = Column(Integer, ForeignKey('planets.uid'))
+    species_uid = Column(Integer, ForeignKey('species.uid'))
+
+class User(Base):
+    __tablename__ = 'user'
+    uid = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=True)
+    created = Column(String(50))
+           
 class People(Base):
     __tablename__ = 'people'
     uid = Column(Integer, primary_key=True)
     _id = Column(String(50), nullable=False)
     name = Column(String(250), nullable=False)
-    height = Column(float)
+    # height = Column(float)
     mass = Column(Integer)
     hair_color = Column(String(20))
     skin_color = Column(String(20))
@@ -21,12 +35,12 @@ class People(Base):
     gender = Column(String(20))
     created = Column(String(50))
     edited = Column(String(50))
-    homeworld = Column(String(50))
+    homeworld = Column(String(50), ForeignKey("planets.uid"))
     url = Column(String(50), nullable=False)
     description = Column(String(100))
 
 class Planets(Base):
-    __tablename__ = 'address'
+    __tablename__ = 'planets'
     uid = Column(Integer, primary_key=True)
     _id = Column(String(50), nullable=False)
     name = Column(String(250), nullable=False)
@@ -40,6 +54,7 @@ class Planets(Base):
     surface_water = Column(Integer)
     created = Column(String(100))
     description = Column(String(100))
+    url = Column(String(50))
     
 class Species(Base):
     __tablename__ = 'species'
@@ -53,19 +68,13 @@ class Species(Base):
     hair_colors = Column(String(100))
     skin_colors = Column(String(100))
     eye_colors = Column(String(50))
-    homeworld = Column(String(50))
+    homeworld = Column(String(50),ForeignKey("planets.uid"))
     language = Column(String(50))
     people = Column(String(300))
     created = Column(String(50))
     edited = Column(String(50))
     url = Column(String(50))
     description = Column(String(50))
-
-
-
-
-
-
 
     def to_dict(self):
         return {}
